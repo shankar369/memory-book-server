73d8836773ace69a36cae3b889e023b9fd321a26
shankar p <shankar369>
2020-11-01 19:40:41 +0530

    cors

--- server.js
+++ server.js
@@ -27,6 +27,8 @@ if (process.env.NODE_ENV == "development") {
   // app.use(cors({ origin: process.env.CLIENT_URL }));
 }
 
+app.use(cors());
+
 app.use(bodyParser.json());
 
 app.use("/api", mainCategoryRoute);
