f86f034e6346fe0a3c94f437d0122c5c91f63be2
shankar p <shankar369>
2020-11-29 19:23:58 +0530

    logic changes for getting local stored meanings

--- controllers/dictionary.controller.js
+++ controllers/dictionary.controller.js
@@ -1,23 +1,56 @@
 const { errorHandler } = require("../helpers/dbErrorHandler");
 const Dictionary = require("../models/Dictionary");
 
-exports.getMeanings = (req, res) => {
-  Dictionary.find().exec((err, meanings) => {
+exports.getMeaning = (req, res) => {
+  Dictionary.findOne({ word: req.params.word }).exec((err, meaning) => {
     if (err) {
-      return res.status(400).json({ error: errorHandler(err) });
+      return res.status(400).json([]);
     }
-    return res.json(meanings);
+    return res.json([meaning]);
   });
 };
 
-exports.postMeaning = (req, res) => {
-  let meaning = new Dictionary(req.body);
-  meaning.save((err, m) => {
-    if (err) {
-      return res.status(400).json({
-        error: errorHandler(err),
-      });
+exports.getMeanings = (req, res) => {
+  // const sortBy = req.query.sortBy || "word";
+  // const limit = parseInt(req.query.limit) || 5;
+  // const sortOrder = parseInt(req.query.sortOrder) || 1;
+  // console.log("-----reached");
+  Dictionary.find()
+    // .sort([[sortBy, sortOrder]])
+    // .limit(limit)
+    .exec((err, meanings) => {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
+      return res.json(meanings);
+    });
+};
+
+exports.postMeaning = async (req, res) => {
+  const found = await Dictionary.findOne({ word: req.body.word });
+  if (found !== null) {
+    return res.json(found);
+  } else {
+    let meaning = new Dictionary(req.body);
+
+    meaning.save((err, m) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        });
+      }
+      return res.json(m);
+    });
+  }
+};
+
+exports.deleteMeaning = (req, res) => {
+  Dictionary.findOneAndDelete({ word: req.params.word }).exec(
+    (err, meaning) => {
+      if (err) {
+        return res.status(400).json([]);
+      }
+      return res.json([meaning]);
     }
-    return res.json(m);
-  });
+  );
 };
--- routes/dictionary.route.js
+++ routes/dictionary.route.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const {
+  getMeaning,
   getMeanings,
   postMeaning,
 } = require("../controllers/dictionary.controller");
 
+router.get("/dictionary/:word", getMeaning);
 router.get("/dictionary", getMeanings);
 router.post("/dictionary", postMeaning);
 
